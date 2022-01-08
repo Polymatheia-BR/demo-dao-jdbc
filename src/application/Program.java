4feba075d1067b2a72747483a55255d0bf273d16
Victor Ferreira <vffvmode@gmail.com>
2022-01-08 00:11:37 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,8 +15,11 @@ public class Program {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
 		Seller seller = new Seller(1, "Victor", "vffvmode@gmail.com", sdf.parse("17/03/2002"), 2000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
